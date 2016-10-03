cd98058ae617aa014c8d98e5ef3b86156500ed51
root <root@aria>
2016-10-03 15:51:25 +0200

    init

---
+++ init.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+mport os
+import argparse
+import string
+import random
+
+def get_random_string(size=32, chars=string.letters + string.digits + ",;.:-_()="):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def replace_words_in_file(file, values):
+    with open(file, "r") as input_file:
+        input_template = string.Template(input_file.read())
+    if not input_template:
+        return False
+    output_text = input_temaplate.substitute(values)
+    with open(file, "w") as output_file:
+        output_file.write(output_text)
+
+parser = argparse.ArgumentParser(description='Docker lemp stack configurator.')
+parser.add_argument('-hn', '--hostname', help='Host name', required=True)
+parser.add_argument('-dbn', '--dbname', help='Database name', required=False, default="database")
+parser.add_argument('-dbu', '--dbuser', help='Database user', required=False, default="user")
+parser.add_argument('-dbp', '--dbpassword', help='Database password', required=False, default=get_random_string())
+parser.add_argument('-dbrp', '--dbrootpassword', help='Database root password', required=False, default=get_random_string())
+args = parser.parse_args()
+args_dict = var(args)
+ 
+base_path = os.path.realpath(__file__)
+nginx_available = "/" + os.path.join("etc", "nginx", "available", args.hostname)
+nginx_enabled = "/" + os.path.join("etc", "nginx", "enabled", args.hostname)
+for file in ["docker-compose.yml", "nginx.external.conf"]
+    file_path = os.join(base_path, file)
+    replace_words_in_file(file_path, args)
+    if file == "nginx.external.conf":
+        os.symlink(file_path, nginx_available)
+        os.symlink(nginx_available, nginx_enabled)
+
+## show values ##
+print ("Generated configuration with:")
+print ("Host name: %s" % args.hostnamea)
+print ("Database name: %s" % args.dbname)
+print ("Database user: %s" % args.dbuser)
+print ("Database password: %s" % args.dbpassword)
+print ("Database root password: %s" % args.dbrootpassword)
